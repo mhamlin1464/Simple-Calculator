1ba3a91419cbe1f0f63e922db0666a6ce90b07f4
mhamlin1464 <mhamlin0621@gmail.com>
2025-04-05 15:50:51 -0500

    Changed Simple_Calculator

--- Simple_Caclulator.cpp
+++ Simple_Caclulator.cpp
@@ -22,15 +22,15 @@ int Simple_Calculator(int num1, int num2, string op){
 
 int main(){
     cout << setw(20) << "Simple Calculator" << endl;
-    cout << "Please pick an operation: add/subtract/divide/multi" << endl;
+    cout << left<<  "Please pick an operation: add/subtract/divide/multi" << endl;
     string op;
     cin >> op;
-    cout << "What is your first number: ";
+    cout << left << "What is your first number: ";
     int num1;
     cin >> num1;
-    cout << "What is your second number: ";
+    cout<< left << "What is your second number: ";
     int num2;
     cin >> num2;
-    cout << "\n Your solution is " << Simple_Calculator(num1,num2,op);
+    cout << left <<  "\nYour solution is " << Simple_Calculator(num1,num2,op);
     return 0;
 }
