870514967d0149502c3dbc343d6f58c5d6de0844
mhamlin1464 <mhamlin0621@gmail.com>
2025-04-09 17:02:59 -0500

    Updated file to conintue runnning until false

--- Simple_Caclulator.cpp
+++ Simple_Caclulator.cpp
@@ -23,15 +23,27 @@ int Simple_Calculator(int num1, int num2, string op){
 int main(){
     cout << setw(20) << "Simple Calculator" << endl;
     cout << setw(20) << "-----------------------" << endl;
-    cout << left<<  "Please pick an operation: add/subtract/divide/multi" << endl;
-    string op;
-    cin >> op;
-    cout << left << "What is your first number: ";
-    int num1;
-    cin >> num1;
-    cout<< left << "What is your second number: ";
-    int num2;
-    cin >> num2;
-    cout << left <<  "\nYour solution is " << Simple_Calculator(num1,num2,op);
+    bool running = true;
+    while (running){
+        cout << setw(20) << "\nWould you like to continue with an operation: Y/N"<< endl;
+        string question;
+        cin >> question;
+        if (question == "Y"){
+            cout << left<<  "Please pick an operation: add/subtract/divide/multi" << endl;
+            string op;
+            cin >> op;
+            cout << left << "What is your first number: ";
+            int num1;
+            cin >> num1;
+            cout<< left << "What is your second number: ";
+            int num2;
+            cin >> num2;
+            cout << left <<  "\nYour solution is " << Simple_Calculator(num1,num2,op);
+        }
+        else{
+            cout << setw(20) <<  "\nThank you! Have a good day!" << endl;
+            running = false;
+        }
+    }
     return 0;
 }
